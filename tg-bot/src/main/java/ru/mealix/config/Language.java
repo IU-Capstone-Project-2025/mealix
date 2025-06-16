77d19eaea124b128ff86266ca08de90e0d0cd87a
GG_Host <gd.host@yandex.ru>
2025-06-16 23:42:53 +0300

    feat: update Language implementation

--- tg-bot/src/main/java/ru/mealix/config/Language.java
+++ tg-bot/src/main/java/ru/mealix/config/Language.java
@@ -1,6 +1,7 @@
 package ru.mealix.config;
 
 import lombok.Getter;
+import org.telegram.telegrambots.meta.api.objects.Update;
 
 @Getter
 public enum Language {
@@ -11,5 +12,10 @@ public enum Language {
 
     Language(String code) {
         this.code = code;
+    }
+
+    public static Language fromUpdate(Update update) {
+        String languageCode = update.getCallbackQuery().getFrom().getLanguageCode();
+        return Language.valueOf(languageCode);
     }
 }
