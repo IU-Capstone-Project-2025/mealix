1a91f504c05f91c32871a0bb4a3c460bea6e47a7
GG_Host <gd.host@yandex.ru>
2025-06-17 00:27:57 +0300

    feat: implement update handling

--- tg-bot/src/main/java/ru/mealix/config/BotCore.java
+++ tg-bot/src/main/java/ru/mealix/config/BotCore.java
@@ -1,19 +1,29 @@
 package ru.mealix.config;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
 import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+import ru.mealix.command.Command;
+import ru.mealix.handler.ChainNode;
 
 import java.util.List;
+import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.Executor;
 
 @Service
+@Slf4j
 public class BotCore extends TelegramLongPollingBot {
     private final Executor botAsyncExecutor;
+    private final ConcurrentHashMap<Language, ConcurrentHashMap<String, Command>> commands;
+    private final ChainNode handler;
 
-    public BotCore(String botToken, Executor botAsyncExecutor) {
+    public BotCore(String botToken, Executor botAsyncExecutor, ConcurrentHashMap<Language, ConcurrentHashMap<String, Command>> commands, ChainNode handler) {
         super(botToken);
         this.botAsyncExecutor = botAsyncExecutor;
+        this.commands = commands;
+        this.handler = handler;
     }
 
     @Override
@@ -27,7 +37,13 @@ public class BotCore extends TelegramLongPollingBot {
 
     @Override
     public void onUpdateReceived(Update update) {
-
+        try {
+            handler.handle(update, this);
+        } catch (TelegramApiException e) {
+            log.error("Error handling update: {}", e.getMessage());
+        } catch (RuntimeException e) {
+            log.error("Unknown error: {}", e.getMessage());
+        }
     }
 
     @Override
