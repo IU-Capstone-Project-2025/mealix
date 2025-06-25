c8d4f4f35d56b504ef2ce85b6b15ed11529104b4
GG_Host <gd.host@yandex.ru>
2025-06-26 00:26:56 +0300

    fix: hot fix to connect services

--- backend/src/main/java/ru/backend/config/RequestConfig.java
+++ backend/src/main/java/ru/backend/config/RequestConfig.java
@@ -4,8 +4,13 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpStatusCode;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
+import org.springframework.http.client.SimpleClientHttpRequestFactory;
+import org.springframework.web.client.RestClient;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Mono;
+
+import java.time.Duration;
 
 @Configuration
 public class RequestConfig {
@@ -14,12 +19,18 @@ public class RequestConfig {
     private String mlUrl;
 
     @Bean
-    public WebClient mlClient() {
-        return WebClient.builder()
+    public RestClient mlClient() {
+        return RestClient.builder()
                 .baseUrl(mlUrl)
-                .defaultStatusHandler(HttpStatusCode::is4xxClientError,
-                        r -> Mono.error(new RuntimeException(r.statusCode().toString())))
+                .requestFactory(simpleClientHttpRequestFactory())
                 .build();
     }
 
+    @Bean
+    public SimpleClientHttpRequestFactory simpleClientHttpRequestFactory() {
+        SimpleClientHttpRequestFactory factory = new SimpleClientHttpRequestFactory();
+        factory.setConnectTimeout(Duration.ofMinutes(5));
+        factory.setReadTimeout(Duration.ofMinutes(5));
+        return factory;
+    }
 }
--- tg-bot/src/main/java/ru/mealix/config/RequestConfig.java
+++ tg-bot/src/main/java/ru/mealix/config/RequestConfig.java
@@ -1,24 +1,32 @@
 package ru.mealix.config;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpStatusCode;
+import org.springframework.web.client.RestClient;
+import org.springframework.web.reactive.function.client.ExchangeFilterFunction;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Mono;
 
 @Configuration
+@Slf4j
 public class RequestConfig {
 
     @Value("${backend.host}")
     private String backendHost;
 
     @Bean(name = "userClient")
-    public WebClient userClient() {
-        return WebClient.builder()
-                .baseUrl(backendHost + "/user")
-                .defaultStatusHandler(HttpStatusCode::is4xxClientError, r -> Mono.error(new RuntimeException(r.statusCode().toString())))
-                .build();
+    public RestClient userClient() {
+        return RestClient.create(backendHost + "/user");
+    }
+
+    private ExchangeFilterFunction logRequest() {
+        return ExchangeFilterFunction.ofRequestProcessor(clientRequest -> {
+            log.info("Sending request: {} {}", clientRequest.method(), clientRequest.url());
+            return Mono.just(clientRequest);
+        });
     }
 
 
