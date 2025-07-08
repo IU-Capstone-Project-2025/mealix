0d6559c77cd4830a308df4e7623d6efa974fb955
GG_Host <gd.host@yandex.ru>
2025-07-09 01:00:41 +0300

    refactor: add javadoc

--- backend/src/main/java/ru/backend/service/UserService.java
+++ backend/src/main/java/ru/backend/service/UserService.java
@@ -7,6 +7,9 @@ import ru.backend.dto.UserDto;
 import ru.backend.exception.NotFoundException;
 import ru.backend.repository.UserRepository;
 
+/**
+ * Service for work with users.
+ */
 @Service
 @RequiredArgsConstructor
 @Transactional
@@ -14,10 +17,22 @@ public class UserService {
 
     private final UserRepository userRepository;
 
+
+    /**
+     * Saves user to database.
+     * @param userDto Dto with user data.
+     */
     public void saveUser(UserDto userDto) {
         userRepository.save(userDto.toEntity());
     }
 
+
+    /**
+     * Gets user by id.
+     * @param userId id of user.
+     * @return UserDto with user data.
+     * @throws NotFoundException if user not found.
+     */
     public UserDto getUser(Long userId) {
         return userRepository
                 .findById(userId)
